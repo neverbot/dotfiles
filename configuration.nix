022f018d643c7ede254e895097a39382ea97de3c
neverbot <ivan@neverbot.com>
2024-05-14 12:32:05 +0200

    chore(nixos): wayland with gdm

--- configuration.nix
+++ configuration.nix
@@ -54,10 +54,12 @@
     # Enable the X11 windowing system.
     enable = true;
 
-    displayManager.sddm.enable = true;    
+    # the display manager handles user login
+    # compatible with wayland: gdm, sddm
+    # displayManager.sddm.enable = true;
+    displayManager.gdm.enable = true;
 
-    # Enable the GNOME Desktop Environment.
-    # displayManager.gdm.enable = true;
+    # Enable the GNOME Desktop Environment
     # desktopManager.gnome.enable = true;
 
     # Configure keymap in X11
@@ -65,7 +67,7 @@
     xkbVariant = "";
 
     libinput = {
-      # Enable touchpad support (enabled default in most desktopManager).
+      # Enable touchpad support (enabled default in most desktopManager)
       enable = true;
       touchpad = {
         naturalScrolling = false;
@@ -92,11 +94,11 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    # jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    # media-session.enable = true;
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
