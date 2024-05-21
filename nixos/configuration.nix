fc0941e08f6681bd81285e681a75843868968ef8
neverbot <ivan@neverbot.com>
2024-05-21 20:35:07 +0200

    undo

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -199,7 +199,6 @@
       git 
       gh              # github cli 
       lynx            # text-mode web browser
-      killall         # kill processes by name
       
       # hardware info
       lshw pciutils inxi glxinfo vulkan-tools 
